174a307d9f6636e973a63497b2a08cf60284edbf
OperatorSigmaM <ana.pavlin@bk.ru>
2026-01-01 15:57:04 +0200

    Clean up mess and fix build error 126

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,6 @@
-const sitemap = require("@quasibit/eleventy-plugin-sitemap");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(sitemap, {
-    sitemap: {
-      hostname: "https://freelance-tax-calculator-lime.vercel.app",
-    },
-  });
-  
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/googlef689d6ec7277c7f1.html");
   
