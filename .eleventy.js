d4f7f6ed2622e1fd008bad0b009a7da457e4ea1b
OperatorSigmaM <ana.pavlin@bk.ru>
2026-01-01 16:26:34 +0200

    Remove sitemap plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,21 @@
-const sitemap = require("@quasibit/eleventy-plugin-sitemap");
-
 module.exports = function(eleventyConfig) {
-  // Авто-генерация сайтмапа
-  eleventyConfig.addPlugin(sitemap, {
-    sitemap: {
-      hostname: "https://freelance-tax-calculator-lime.vercel.app",
-    },
-  });
-
-  // Копируем статику
+  // Копируем папки и файлы напрямую в итоговый сайт
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/googlef689d6ec7277c7f1.html");
+  
+  // Если у тебя есть картинки или другие папки, добавь их сюда так же
   
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes"
-    }
+    },
+    // Указываем, что работаем с Nunjucks (njk)
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
   };
 };
